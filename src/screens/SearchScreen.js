bc442ec57b2fb6256295b2f0799d6739971a8292
Morshed <morshedalamustc22@gmail.com>
2019-10-22 20:21:57 +0600

    initial commit

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View,Text, StyleSheet, Button  } from 'react-native';
+
+const SearchScreen = () => {
+    return (
+        <View>
+           <Text>Search Screen</Text>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    text: {
+        fontSize: 30
+    },
+
+});
+
+export default SearchScreen;
